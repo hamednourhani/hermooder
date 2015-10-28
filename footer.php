0dfa3bb4af3a7d0529ddacfeeb34aaf86e613e35
Hamed Nourhani <info@itstar.ir>
2015-10-28 15:03:34 +0330

    first commit

---
+++ footer.php
@@ -0,0 +1,62 @@
+<!-- ********************************************************************* -->
+	<!--****************** Site footer      ***********************************-->
+	<!-- ********************************************************************* -->
+
+
+			<?php get_sidebar('footer-first'); ?>
+			
+			
+			<div class="related-companies">
+				<section class="layout">
+					<div class="co-container">
+						<img src="<?php echo get_template_directory_uri().'/images/woda.png';?>"/>
+					</div>
+					
+					<div class="co-container">
+						<img src="<?php echo get_template_directory_uri().'/images/tejarat.png';?>"/>
+					</div>
+					
+					<div class="co-container">
+						<img src="<?php echo get_template_directory_uri().'/images/eleci.png';?>"/>
+					</div>
+					
+					<div class="co-container">
+					
+					</div>
+
+					<div class="co-container">
+						<img src="<?php echo get_template_directory_uri().'/images/nic.png';?>"/>
+					</div>
+					
+					<div class="co-container">
+						<img src="<?php echo get_template_directory_uri().'/images/icann.png';?>"/>
+					</div>
+					
+					<div class="co-container">
+						<img src="<?php echo get_template_directory_uri().'/images/onlinenic.png';?>"/>
+					</div>
+				</section>
+			</div>
+
+			<footer class="site-footer">
+				<section class="layout">
+					<div class="social-links">
+						<a class="facebook-link" href="http://www.facebook.com/karait.sitedesign"></a>
+						<a class="twitter-link" href="http://www.twitter.com/kara_sitedesign"></a>
+						<a class="gplus-link" href="http://google.com/+Karaitsitedesign"></a>
+						<a class="linkedin-link" href="http://www.linkedin.com/company/farakaranet"></a>
+					</div>
+				</section>
+			</footer> <!-- footer -->
+	
+	
+	
+
+	<!-- scrolltofixed menu -->
+	
+	
+		</div>
+
+		<?php wp_footer(); ?>
+	</body><!-- body -->
+</html><!-- html -->
